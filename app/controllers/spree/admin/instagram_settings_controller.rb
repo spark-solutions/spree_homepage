e55e011a5a9ec3e05fa16ba3c5028a83bfd8af52
igor_b <bezsmertnyi.igor@gmail.com>
2019-05-14 12:53:25 +0200

    Feature/add admin panel scaffold

---
+++ app/controllers/spree/admin/instagram_settings_controller.rb
@@ -0,0 +1,10 @@
+module Spree
+  module Admin
+    class InstagramSettingsController < Spree::Admin::BaseController
+      include Spree::Backend::Callbacks
+
+      def edit; end
+      def update; end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Spree::Core::Engine.add_routes do
   # Add your extension routes here
+  namespace :admin do
+    resource :home_page_settings, only: %i[edit update]
+    resource :instagram_settings, only: %i[edit update]
+  end
 end
