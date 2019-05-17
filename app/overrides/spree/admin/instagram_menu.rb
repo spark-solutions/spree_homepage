fc710f5422b45d5de2d8439a14023242aab07f68
Paweł Kurzyca <pawel.kurzyca@sparksolutions.co>
2019-05-17 09:51:34 +0200

    Update sidebar links

--- app/overrides/spree/admin/instagram_menu.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-  virtual_path: 'spree/admin/shared/sub_menu/_configuration',
-  name: 'Add instagram tab',
-  insert_bottom: 'ul[data-hook="admin_configurations_sidebar_menu"]',
-  text: '<%= configurations_sidebar_menu_item(Spree.t(:instagram), spree.edit_admin_instagram_settings_path) %>'
-)
