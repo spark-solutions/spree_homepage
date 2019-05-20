ec56c8fdebede232461bb778348c7cb3cd57a75c
tniezg <tomek.niezgoda@mail.com>
2019-05-20 17:25:16 +0200

    Switch to using encrypted credentials

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,4 +1,4 @@
 Instagram.configure do |config|
-  config.client_id = Rails.application.secrets.instagram_client_id
-  config.client_secret = Rails.application.secrets.instagram_client_secret
+  config.client_id = Rails.application.credentials.instagram_client_id
+  config.client_secret = Rails.application.credentials.instagram_client_secret
 end
