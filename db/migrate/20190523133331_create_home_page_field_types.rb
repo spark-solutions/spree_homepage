cdeaf8753bdb0e6c820a8da55244c0207c1d0ed4
Paweł Kurzyca <pawel.kurzyca@sparksolutions.co>
2019-05-23 16:57:07 +0200

    Associate section and fields with types

--- db/migrate/20190523133331_create_home_page_field_types.rb
+++ db/migrate/20190523133331_create_home_page_field_types.rb
@@ -3,7 +3,7 @@ class CreateHomePageFieldTypes < ActiveRecord::Migration[5.2]
     create_table :spree_home_page_field_types do |t|
       t.string :name, null: false
       t.string :type, null: false
-      t.references :spree_home_page_section_type, index: { name: 'index_field_types_on_section_type_id' }, foreign_key: true
+      t.references :home_page_section_type, index: { name: 'index_field_types_on_section_type_id' }
       t.timestamps
     end
   end
