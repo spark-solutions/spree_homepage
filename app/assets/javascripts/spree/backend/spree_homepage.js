21d2caa23f3aece797ccee3336b3962afd03eee2
Paweł Kurzyca <pawel.kurzyca@sparksolutions.co>
2019-05-24 15:53:36 +0200

    Filter field types for selected section

--- app/assets/javascripts/spree/backend/spree_homepage.js
+++ app/assets/javascripts/spree/backend/spree_homepage.js
@@ -1,22 +1,52 @@
 //= require spree/backend
 
-$(function () {
+$(function() {
   var elements = {
-    fieldType: $('#home_page_field_field_type'),
     content: $('#home_page_field_content_field'),
-    image: $('#home_page_field_image_field')
+    image: $('#home_page_field_image_field'),
+    section: $('#home_page_field_home_page_section_id'),
+    fieldType: $('#home_page_field_home_page_field_type_id')
   }
-  function switchView() {
-    if (elements.fieldType.val() === 'text') {
+
+  function filterFieldTypes() {
+    var fieldTypeIds = elements.section
+      .find(':selected')
+      .data('homePageFieldTypes')
+    var fieldTypeIdsTable = String(fieldTypeIds).split(' ')
+    elements.fieldType.find('option').each(function(id, option) {
+      var $option = $(option)
+      var value = $option.val()
+      if (fieldTypeIdsTable.indexOf(value) === -1) {
+        $option.attr('disabled', 'disabled')
+      } else {
+        $option.removeAttr('disabled')
+      }
+    })
+  }
+
+  function switchContentView() {
+    if (elements.fieldType.find(':selected').data('type') === 'text') {
       elements.image.hide()
       elements.content.show()
     } else {
-      elements.image.show ()
+      elements.image.show()
       elements.content.hide()
     }
   }
-  if (elements.fieldType) {
-    switchView()
-    elements.fieldType.on('change', switchView)
+
+  function onSectionChange() {
+    elements.fieldType.val(null).trigger('change')
+    filterFieldTypes()
+  }
+
+  function onFieldTypeChange() {
+    switchContentView()
+  }
+
+  if (elements.fieldType.length) {
+    filterFieldTypes()
+    switchContentView()
+    elements.section.on('change', onSectionChange)
+    elements.fieldType.on('change', onFieldTypeChange)
   }
 })
