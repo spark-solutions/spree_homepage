1dcd2984a32e21361f340d89240c11be8e94a7b0
Paweł Kurzyca <pawel.kurzyca@sparksolutions.co>
2019-05-24 17:07:36 +0200

    Add missing resource alert

--- app/controllers/spree/admin/home_page_sections_controller.rb
+++ app/controllers/spree/admin/home_page_sections_controller.rb
@@ -1,6 +1,13 @@
 module Spree
   module Admin
     class HomePageSectionsController < ResourceController
+      before_action :section_types, only: %i[new edit]
+
+      private
+
+      def section_types
+        @section_types ||= Spree::HomePageSectionType.all
+      end
     end
   end
 end
