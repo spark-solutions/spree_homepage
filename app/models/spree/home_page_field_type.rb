e6bea730658983a390f7d338f74c97544c3d4a3d
Paweł Kurzyca <pawel.kurzyca@sparksolutions.co>
2019-05-24 17:08:12 +0200

    Add name presence validation for types

--- app/models/spree/home_page_field_type.rb
+++ app/models/spree/home_page_field_type.rb
@@ -9,6 +9,7 @@ module Spree
     has_many :home_page_types
     has_many :home_page_section_types, through: :home_page_types
 
+    validates :name, presence: true, uniqueness: true
     validates :field_type, presence: true
   end
 end
--- app/models/spree/home_page_section_type.rb
+++ app/models/spree/home_page_section_type.rb
@@ -3,5 +3,7 @@ module Spree
     has_many :home_page_types
     has_many :home_page_field_types, through: :home_page_types
     has_many :home_page_sections
+
+    validates :name, presence: true, uniqueness: true
   end
 end
