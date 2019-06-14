81898c972b47bb50ef5432a090879a24802b1af1
Paweł Kurzyca <pawel.kurzyca@sparksolutions.co>
2019-06-14 08:07:50 +0200

    Add user param to instagram posts api endpoint

--- app/controllers/spree/api/v2/storefront/instagram_posts_controller.rb
+++ app/controllers/spree/api/v2/storefront/instagram_posts_controller.rb
@@ -4,13 +4,18 @@ module Spree
       module Storefront
         class InstagramPostsController < Spree::Api::V2::BaseController
           def index
+            resource = params[:user] ? user_posts : hashtag_posts
             render_serialized_payload { serialize_resource(resource) }
           end
 
           private
 
-          def resource
-            Spree::InstagramPost.where(show: true)
+          def user_posts
+            Spree::InstagramPost.user.where(show: true)
+          end
+
+          def hashtag_posts
+            Spree::InstagramPost.hashtag.where(show: true)
           end
 
           def resource_serializer
