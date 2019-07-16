b5d729cced4c3835365e304f4ba46776f78cb4c2
Paweł Kurzyca <pawel.kurzyca@sparksolutions.co>
2019-07-16 13:23:20 +0200

    Add caption and instagram links in admin

--- app/helpers/spree/admin/instagram_posts_helper.rb
+++ app/helpers/spree/admin/instagram_posts_helper.rb
@@ -1,8 +1,18 @@
 module Spree
   module Admin
     module InstagramPostsHelper
-      def thumbnail_url(post)
-        JSON.parse(post.data)['media_url']
+      def instagram_data(post)
+        JSON.parse(post.data)
+      end
+
+      def instagram_link(post, content)
+        link_to content, instagram_data(post)["permalink"]
+      end
+
+      def instagram_thumbnail(post)
+        data = instagram_data(post)
+        tag = data["media_type"] == "VIDEO" ? :video : :img
+        content_tag(tag, nil, src: data["media_url"], class: "thumbnail")
       end
     end
   end
