fdcb2bd3973c057e5d0bf7d14c3e62056738be92
Paweł Kurzyca <pawel.kurzyca@sparksolutions.co>
2019-07-17 11:38:14 +0200

    Change instagram connected field instagram_business_account => connected_instagram_account

--- app/services/instagram/get_access_token.rb
+++ app/services/instagram/get_access_token.rb
@@ -22,9 +22,9 @@ module Instagram
 
     def user_id
       graph = Koala::Facebook::API.new(@access_token)
-      accounts = graph.get_object("me?fields=accounts{instagram_business_account,name}") { |data| data["accounts"] }
-      page = accounts["data"].find { |account| account["name"] == ENV["FB_PAGE_NAME"] }
-      page && page["instagram_business_account"] ? page["instagram_business_account"]["id"] : ""
+      accounts = graph.get_object("me?fields=accounts{connected_instagram_account,name}") { |data| data["accounts"] }
+      page = accounts["data"].find { |account| account["name"] == ENV["FB_PAGE_NAME"] } || accounts["data"].first 
+      page && page["connected_instagram_account"] ? page["connected_instagram_account"]["id"] : ""
     end
 
     def set_user_id
