68266663cb45ad2cdacf18aaadd0e49a4565dc67
Tomasz Niezgoda <tomasz.niezgoda@sparksolutions.co>
2020-02-26 15:42:01 +0100

    [FN-1] Save user without post_id and ignore image attachment until build_image and create_image

--- app/controllers/spree/admin/instagram_users_controller.rb
+++ app/controllers/spree/admin/instagram_users_controller.rb
@@ -2,13 +2,17 @@ module Spree
   module Admin
     class InstagramUsersController < ResourceController
       def create
-        @instagram_user = Spree::InstagramUser.new(instagram_user_params)
+        @instagram_user = Spree::InstagramUser.new(instagram_user_basic_params)
         @instagram_user.build_image(attachment: image_params) if image_params
-        @instagram_user.instagram_posts << Spree::InstagramPost.find(post_id_param) if post_id_param
+        @instagram_user.instagram_posts << Spree::InstagramPost.find(post_id_param) if post_id_param.present?
 
         if @instagram_user.save
           flash[:success] = flash_message_for(@instagram_user, :successfully_created)
-          redirect_to hashtag_admin_instagram_posts_path
+          if post_id_param.empty?
+            redirect_to edit_admin_instagram_user_path(@instagram_user)
+          else
+            redirect_to hashtag_admin_instagram_posts_path
+          end
         else
           flash[:error] = Spree.t(:could_not_create_instagram_user)
           render :new
@@ -18,7 +22,7 @@ module Spree
       def update
         @instagram_user = Spree::InstagramUser.find(params[:id])
         @instagram_user.create_image(attachment: image_params) if image_params
-        if @instagram_user.update(instagram_user_params)
+        if @instagram_user.update(instagram_user_basic_params)
           flash[:success] = flash_message_for(@instagram_user, :successfully_updated)
         else
           flash[:error] = Spree.t(:could_not_update_instagram_user)
@@ -27,6 +31,10 @@ module Spree
       end
 
       private
+
+      def instagram_user_basic_params
+        instagram_user_params.slice(:name, :username)
+      end
 
       def image_params
         params[:instagram_user][:image]
@@ -37,7 +45,7 @@ module Spree
       end
 
       def instagram_user_params
-        params.require(:instagram_user).permit(:name, :username)
+        params.require(:instagram_user).permit(:name, :username, :image)
       end
     end
   end
